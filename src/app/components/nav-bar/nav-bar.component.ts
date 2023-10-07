d40c08b99a56d621eb4b38503a5f895d9941964e
Elias Salazar <eiriselias@gmail.com>
2023-10-07 16:52:28 -0500

    se agrego el modo oscuro y se anexo el archivo equipos.json

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -5,7 +5,17 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.scss']
 })
-export class NavBarComponent{
-
+export class NavBarComponent implements OnInit{
+  constructor(){
+    
+  }
+  ngOnInit(): void {
+    const modo = document.querySelector(".modo");
+    modo?.addEventListener("click",()=>{
+      document.body.classList.toggle("dark");
+      document.querySelector(".oscuro")?.classList.toggle("ver")
+      document.querySelector(".luz")?.classList.toggle("ver")
+    })
+  }
 }
 
